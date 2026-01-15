8fc0b879da170d23a7b8e83c6ec4000e6369b2f1
bangohan34 <taikinatsume34@gmail.com>
2026-01-15 18:55:46 +0900

    エラー

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ st.markdown(c.hide_streamlit_style, unsafe_allow_html=True)
 # --- データの準備 ---
 df = u.load_kakeibo_data()
 df_crypto = u.load_crypto_data()
-tody = '2028-12-20'
+today = '2028-12-20'
 # today = pd.to_datetime("today").normalize()
 
 # --- 入力フォーム ---
