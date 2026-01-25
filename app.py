eb8a29b34aa14cbccd4aff084eba616ab0b7dc47
bangohan34 <taikinatsume34@gmail.com>
2026-01-25 19:29:18 +0900

    バーの色

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ if(url_user_id =="u1"):
         SYMBOL_COLORS = {
             'BTC':'#F4B400',
             'ETH':'#9079ad',
-            'XRP':'#afafb0',
+            'XRP':"#6161ec",
             'IOST':'#00c8c8',
             'PI':'#9600ff'
         }
