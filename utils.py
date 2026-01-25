1924bddbef9e6ec42530474fe70f9939533200ae
bangohan34 <taikinatsume34@gmail.com>
2026-01-25 21:19:39 +0900

    ちょっと

--- utils.py
+++ utils.py
@@ -259,7 +259,7 @@ def create_expense_pie_chart(data):
         outerRadius=90,
         stroke="#d3d3d3"
         ).properties(
-            height=200
+            height=150
         )
     # グラフの設定（背景透明、文字色など）
     return pie.configure_view(
